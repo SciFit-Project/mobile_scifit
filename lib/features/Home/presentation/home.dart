5a757ee621fe621e7a153a942444fe0cadf06cd1
tony219y <aekanan2004@gmail.com>
2026-02-14 13:20:12 +0700

    feat: Nav route (Home)

---
+++ lib/features/Home/presentation/home.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _Home();
+}
+
+class _Home extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
---
+++ lib/features/navigation/presentation/main_nav.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_scifit/features/Home/presentation/home.dart';
+
+class MainNav extends StatefulWidget {
+  const MainNav({super.key});
+
+  @override
+  State<MainNav> createState() => _MainNavState();
+}
+
+class _MainNavState extends State<MainNav> {
+  int _currentIndex = 0;
+
+  // รายการหน้าจอที่ดึงมาจาก Feature ต่างๆ
+  // final List<Widget> _screens = const [HomePage(), SearchPage(), ProfilePage()];
+  final List<Widget> _screens = const [HomePage()];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(index: _currentIndex, children: _screens),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) => setState(() => _currentIndex = index),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+      ),
+    );
+  }
+}
