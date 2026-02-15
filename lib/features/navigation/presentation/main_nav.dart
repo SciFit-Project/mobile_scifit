595642ff3c9876e8583008a41c8674280b15170e
tony219y <aekanan2004@gmail.com>
2026-02-15 15:32:16 +0700

    feat: load permission health

--- lib/features/navigation/presentation/main_nav.dart
+++ lib/features/navigation/presentation/main_nav.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mobile_scifit/features/Home/presentation/home.dart';
 import 'package:mobile_scifit/features/Sleep/presentation/main_sleep.dart';
 import 'package:mobile_scifit/features/Steps/presentation/main_steps.dart';
+import 'package:mobile_scifit/services/main_health_services.dart';
 
 class MainNav extends StatefulWidget {
   const MainNav({super.key});
@@ -11,9 +12,28 @@ class MainNav extends StatefulWidget {
 }
 
 class _MainNavState extends State<MainNav> {
+  final MainHealthServices _services = MainHealthServices();
   int _currentIndex = 0;
-
   final List<Widget> _screens = const [HomePage(), StepPage(), SleepPage()];
+
+  Future<void> loadSteps() async {
+    bool granted = await _services.reqPermission();
+    if (!granted) {
+      return;
+    }
+  }
+
+  Future<void> setupHealth() async {
+    await _services.init();
+    await loadSteps();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupHealth();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
